d4a0f5ccea6f5f13662f4b4b4a3a7e03b473c8a9
Piyush Prajapati <140145139+piyush0213@users.noreply.github.com>
2025-02-16 14:11:43 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,30 +4,23 @@ int main() {
     int num1, num2;
     char operator;
 
-    // Read input from the user
+    // Taking input in the format: num1 operator num2
     scanf("%d %c %d", &num1, &operator, &num2);
 
-    // Perform the operation based on the operator
-    switch (operator) {
-        case '+':
-            printf("%d %c %d = %d\n", num1, operator, num2, num1 + num2);
-            break;
-        case '-':
-            printf("%d %c %d = %d\n", num1, operator, num2, num1 - num2);
-            break;
-        case '*':
-            printf("%d %c %d = %d\n", num1, operator, num2, num1 * num2);
-            break;
-        case '/':
-            if (num2 != 0) {
-                printf("%d %c %d = %d\n", num1, operator, num2, num1 / num2);
-            } else {
-                printf("Error\n");
-            }
-            break;
-        default:
+    if (operator == '+') {
+        printf("%d\n", num1 + num2);
+    } else if (operator == '-') {
+        printf("%d\n", num1 - num2);
+    } else if (operator == '*') {
+        printf("%d\n", num1 * num2);
+    } else if (operator == '/') {
+        if (num2 != 0) {
+            printf("%d\n", num1 / num2);
+        } else {
             printf("Error\n");
-            break;
+        }
+    } else {
+        printf("Error\n");
     }
 
     return 0;
